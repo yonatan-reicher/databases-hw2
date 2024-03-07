67e9d2e55bb4e6f6b28bb39e4f0f7c7c5079b687
Bar Tzipori <bar.tzipori@gmail.com>
2024-03-08 00:54:06 +0200

    added: getters, add and delet functions for owner and apartment

--- Solution.py
+++ Solution.py
@@ -65,15 +65,19 @@ def drop_tables():
 
 
 def add_owner(owner: Owner) -> ReturnValue:
+    if not isinstance(owner, Owner):
+        return ReturnValue.BAD_PARAMS
     conn = None
     try:
         conn = Connector.DBConnector()
-        owner_name = conn.execute("SELECT name FROM owner WHERE id = %s", (owner_id,))
-        if owner_name is not None:
-            return Owner(owner_id, owner_name)
-        else:
-            # TODO: Check if bad_owner is an actual func written by the course staff
-            return Owner.bad_owner()
+        query = sql.SQL("INSERT INTO Owner(id, name) VALUES({ownerid}, {ownername})").format(
+            ownerid=sql.Literal(owner.get_owner_id()),
+            ownername=sql.Literal(owner.get_owner_name()))
+        rows_effected, _ = conn.execute(query)
+        if rows_effected == 1:
+            return ReturnValue.OK
+        if rows_effected == 0:
+            return ReturnValue.ALREADY_EXISTS
     except DatabaseException.ConnectionInvalid as e:
         print(e)
     except DatabaseException.NOT_NULL_VIOLATION as e:
@@ -96,11 +100,11 @@ def get_owner(owner_id: int) -> Owner:
     conn = None
     try:
         conn = Connector.DBConnector()
-        owner_name = conn.execute("SELECT name FROM owner WHERE id = %s", (owner_id,))
+        query = sql.SQL("SELECT name FROM owner WHERE id = {ownerid}").format(ownerid=sql.Literal(owner_id))
+        rows_effected, owner_name = conn.execute(query)
         if owner_name is not None:
             return Owner(owner_id, owner_name)
         else:
-            # TODO: Check if bad_owner is an actual func written by the course staff
             return Owner.bad_owner()
     except DatabaseException.ConnectionInvalid as e:
         print(e)
@@ -116,18 +120,20 @@ def get_owner(owner_id: int) -> Owner:
         print(e)
     finally:
         # will happen any way after try termination or exception handling
-        if conn: conn.close()
+        if conn:
+            conn.close()
     pass
 
 
 def delete_owner(owner_id: int) -> ReturnValue:
-    #for delete funcs, params value can and should be tested via python.
+    # for delete funcs, params value can and should be tested via python.
     if not isinstance(owner_id, int):
         return ReturnValue.BAD_PARAMS
     conn = None
     try:
         conn = Connector.DBConnector()
-        rows_effected = conn.execute("DELETE FROM owner WHERE id = %s", (owner_id,))
+        query = sql.SQL("DELETE FROM owner WHERE id = {ownerid}").format(ownerid=sql.Literal(owner_id))
+        rows_effected = conn.execute(query)
         if rows_effected is not 0:
             # Owner exists and deleted one or more times
             return ReturnValue.OK
@@ -148,25 +154,107 @@ def delete_owner(owner_id: int) -> ReturnValue:
         print(e)
     finally:
         # will happen any way after try termination or exception handling
-        if conn: conn.close()
-        return ReturnValue.ERROR
+        if conn:
+            conn.close()
     pass
 
 
 def add_apartment(apartment: Apartment) -> ReturnValue:
-    # TODO: implement
+    if not isinstance(apartment, Apartment):
+        return ReturnValue.BAD_PARAMS
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL("INSERT INTO Apartment(id, address, city, country, size) VALUES({apartmentid}, "
+                        "{apartmentaddress}), {apartmentcity}, {apartmentcountry}, {apartmentsize})").format(
+                                                                apartmentid=sql.Literal(apartment.get_id()),
+                                                                apartmentaddress=sql.Literal(apartment.get_address()),
+                                                                apartmentcity=sql.Literal(apartment.get_city()),
+                                                                apartmentcountry=sql.Literal(apartment.get_country()),
+                                                                apartmentsize=sql.Literal(apartment.get_size()))
+        rows_effected = conn.execute(query)
+        if rows_effected == 1:
+            return ReturnValue.OK
+        if rows_effected == 0:
+            return ReturnValue.ALREADY_EXISTS
+    except DatabaseException.ConnectionInvalid as e:
+        print(e)
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        # will happen any way after try termination or exception handling
+        if conn:
+            conn.close()
     pass
-
 
 def get_apartment(apartment_id: int) -> Apartment:
-    # TODO: implement
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL("SELECT id, address, city, country, size FROM Apartment WHERE id = {apartmentid}").format(
+                                                                                apartmentid=sql.Literal(apartment_id))
+        rows_affected, result = conn.execute(query)
+        if rows_affected is not 0:
+            return Apartment(result[0], result[1], result[2], result[3], result[4])
+        else:
+            return Apartment.bad_apartment()
+    except DatabaseException.ConnectionInvalid as e:
+        print(e)
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        # will happen any way after try termination or exception handling
+        if conn:
+            conn.close()
     pass
-
 
 def delete_apartment(apartment_id: int) -> ReturnValue:
-    # TODO: implement
+    if not isinstance(apartment_id, int):
+        return ReturnValue.BAD_PARAMS
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL("DELETE FROM Apartment WHERE id = {apartmentid}").format(ownerid=sql.Literal(apartment_id))
+        rows_effected = conn.execute(query)
+        if rows_effected is not 0:
+            # Apartment exists and deleted one or more times
+            return ReturnValue.OK
+        else:
+            # Apartment does not exist
+            return ReturnValue.NOT_EXISTS
+    except DatabaseException.ConnectionInvalid as e:
+        print(e)
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        # will happen any way after try termination or exception handling
+        if conn:
+            conn.close()
     pass
-
 
 def add_customer(customer: Customer) -> ReturnValue:
     # TODO: implement
@@ -184,7 +272,8 @@ def delete_customer(customer_id: int) -> ReturnValue:
     pass
 
 
-def customer_made_reservation(customer_id: int, apartment_id: int, start_date: date, end_date: date, total_price: float) -> ReturnValue:
+def customer_made_reservation(customer_id: int, apartment_id: int, start_date: date, end_date: date,
+                              total_price: float) -> ReturnValue:
     # TODO: implement
     pass
 
@@ -194,12 +283,14 @@ def customer_cancelled_reservation(customer_id: int, apartment_id: int, start_da
     pass
 
 
-def customer_reviewed_apartment(customer_id: int, apartment_id: int, review_date: date, rating: int, review_text: str) -> ReturnValue:
+def customer_reviewed_apartment(customer_id: int, apartment_id: int, review_date: date, rating: int,
+                                review_text: str) -> ReturnValue:
     # TODO: implement
     pass
 
 
-def customer_updated_review(customer_id: int, apartmetn_id: int, update_date: date, new_rating: int, new_text: str) -> ReturnValue:
+def customer_updated_review(customer_id: int, apartmetn_id: int, update_date: date, new_rating: int,
+                            new_text: str) -> ReturnValue:
     # TODO: implement
     pass
 
